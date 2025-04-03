ce944805dbf5563d0ea5e6ce3db8cd4e25840c25
thihaaung-plusimpact <thi.ha.aung@plus-impact.co>
2025-04-03 22:11:51 +0700

    post

--- app/Models/State.php
+++ app/Models/State.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class State extends BaseState
 {
+    protected $fillable = ['name_mm', 'name_en', 'flag', 'capital_id', 'is_hot'];
+
     public function cities()
     {
         return $this->belongsToMany(City::class, 'state_city', 'state_id', 'city_id');
